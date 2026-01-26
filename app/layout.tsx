f8f49487257e3b9bee06b433dac9c89cf3026c4b
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-01-26 16:37:25 +0330

    env created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { APP_NAME } from "@/lib/constants";
 
 const inter = Inter({subsets:['latin']})
 
 export const metadata: Metadata = {
-  title: "NextStore",
+  title: `${APP_NAME}`,
   description: "A modern ecommerce platform built with next.js",
 };
 
---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "NextStore"  
+export const APP_DESCRIPTAION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTAION ||
+  "A modern ecommerce platform built with next.js";  
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
