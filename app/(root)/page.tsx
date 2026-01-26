0bfeb25adee4fb5709404c4e2a32357e766ec996
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-01-26 16:24:07 +0330

    root folder created with layout for it

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
+      <h1>hello</h1>
       <Button>Click me</Button>
     </div>
   );
 }
+ 
