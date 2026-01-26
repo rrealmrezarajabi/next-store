9f6798fa04dab67a2d64b775b6e9b848c97b5f7f
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-01-26 17:47:47 +0330

    header created

---
+++ components/shared/header/Header.tsx
@@ -0,0 +1,43 @@
+import { APP_NAME } from "@/lib/constants";
+import { ShoppingCart , UserIcon } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.png"
+              alt="logo"
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart />
+              cart
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href="/sign-in">
+              <UserIcon />
+              sign-in
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
