57a8de840d33b6c2f3f37583ea2c83df34c4eb47
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2026-01-28 17:07:06 +0330

    added types on top of the productlist component

--- components/shared/product/ProductList.tsx
+++ components/shared/product/ProductList.tsx
@@ -1,27 +1,28 @@
+type Product = {
+  name: string;
+  slug: string;
+};
 
-
-const ProductList = ({
-  products,
-  title,
-  limit
-}: {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  products: any;
+type ProductListProps = {
+  products: Product[];
   title?: string;
-  limit?:number
-}) => {
+  limit?: number;
+};
 
-    const limitedProducts = limit ? products.slice(0,limit) : products
+const ProductList = ({ products, title, limit }: ProductListProps) => {
+  const limitedProducts = limit ? products.slice(0, limit) : products;
+
   return (
     <div className="my-10">
-      <h2 className="h2-bold mb-4">{title}</h2>
-      {products.length > 0 ? (
+      {title && <h2 className="h2-bold mb-4">{title}</h2>}
+
+      {limitedProducts.length > 0 ? (
         <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {limitedProducts.map((p: any) => (
-            <div key={p.name}>{p.name}</div>
+          {limitedProducts.map((p) => (
+            <div key={p.slug}>{p.name}</div>
           ))}
         </div>
-      ) : ( 
+      ) : (
         <p>No products found</p>
       )}
     </div>
